a0a0a5ae15ffd72ec0ce04435b30e503e62cd32d
Michal Kozik <michal.kozikowski99@gmail.com>
2021-01-07 18:03:57 +0100

    Wyświetlanie produktów w magazynie w surowy sposób z kolekcji.

--- WPF_projekt/AdminWindow.xaml.cs
+++ WPF_projekt/AdminWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,20 @@ namespace WPF_projekt
     /// </summary>
     public partial class AdminWindow : Window
     {
+        public Collection<Product> Products { get; } = new ObservableCollection<Product>();
+
         public AdminWindow()
         {
             InitializeComponent();
+
+            Products.Add(new Product { Name = "item1", Description = "dgshkjagfdkjhfga", Price = 9.99M, Amount = 10 });
+            Products.Add(new Product { Name = "item2", Description = "dgshkjagfdkjhfga", Price = 19.99M, Amount = 11 });
+            Products.Add(new Product { Name = "item3", Description = "dgshkjagfdkjhfga", Price = 29.99M, Amount = 12 });
+        }
+
+        private void LoadProducts(object sender, RoutedEventArgs e)
+        {
+            MagazineListBox.ItemsSource = Products;
         }
     }
 }
---
+++ WPF_projekt/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_projekt
+{
+    public class Product
+    {
+        //pola
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Amount { get; set; }
+        public string EverythingToString
+        {
+            get { return $"{Name} {Description} {Price} {Amount}"; }
+        }
+
+        //metody
+        public override string ToString()
+        {
+            return $"{Name} {Description} {Price} {Amount}";
+        }
+    }
+}
